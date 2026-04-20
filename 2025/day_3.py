504e8c9e3273282c2f7676d814e4b12dead5bbcb
deepthroadocus <sockpuppetmastaz@proton.me>
2026-04-20 17:00:41 +0200

    Day 3 - part 2

--- 2025/day_3.py
+++ 2025/day_3.py
@@ -15,28 +15,51 @@
 # The total output joltage is the sum of the maximum joltage from each bank, so in this example, the total output joltage is 98 + 89 + 78 + 92 = 357.
 # There are many batteries in front of you. Find the maximum joltage possible from each bank; what is the total output joltage?
 
+    # PART 2 
+# Now, you need to make the largest joltage by turning on exactly twelve batteries within each bank.
+
+# The joltage output for the bank is still the number formed by the digits of the batteries you've turned on; the only difference is that now there will be 12 digits in each bank's joltage output instead of two.
+
+# What is the new total output joltage?
+
 from day_3_input import batteries
 
 def text_to_batteries(text:str) -> list[list[int]] :
     return [[int(char) for char in line] for line in text.splitlines()]
-
-def max_joltage(bank:list[int]) :
-    best_combination = 0 
-    for i in range(len(bank)) :
-        for j in range(i+1, len(bank)) :
-            # Compose a 2 digits number
-            number = bank[i] * 10 + bank[j]
-            best_combination = max(best_combination, number)
-    return best_combination
             
+def max_joltage(bank:list[int], combination_size:int=12) -> int :
+    n = len(bank)
 
-def turn_batteries_on(batteries:str) -> int :
+    if n <= combination_size :
+        return int(''.join(str(num) for num in bank)) # Not enough digits, use them all
+    
+    best_combination = []
+    
+    start = 0 
+    for i in range(combination_size) :
+        digits_remaining = combination_size - 1 - i # Decrement window size each time a digit is added
+
+        end = n - digits_remaining # Leave room for enough digits to bo add
+
+        best_idx = start # Defaults at first item in the window
+
+        # Find best index in bank[start:end]
+        for j in range (start, end) :
+            if bank[j] > bank[best_idx] :
+                best_idx = j # Find max index
+        
+        best_combination.append(bank[best_idx])
+        start = best_idx + 1 # Start at next number in the list
+
+    return int(''.join(str(num) for num in best_combination))
+    
+def turn_batteries_on(batteries:str, digits_amount:int=2) -> int :
     banks = text_to_batteries(batteries)
 
     total = 0
     for bank in banks :
-        total += max_joltage(bank)
+        total += max_joltage(bank, digits_amount)
 
     return total
 
-print(turn_batteries_on(batteries))
+print(turn_batteries_on(batteries, 12))
